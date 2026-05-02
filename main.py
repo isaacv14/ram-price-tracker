c2931e2fe3f86b5a3f1b2c59465d29f612edea23
macRaidev <isaac14e2007@gmail.com>
2026-05-01 21:51:18 -0500

    feat: main.py for importing scrapping.py and cleaning_scrapping file

---
+++ main.py
@@ -0,0 +1,4 @@
+from cleaning_scrapping import mercado_libre_mx
+from scrapping import Scrapping
+
+Scrapping(mercado_libre_mx.url, mercado_libre_mx.html_element, mercado_libre_mx.html_class, mercado_libre_mx.cleaning_function)
