1ede0cb04d03bfe8fda4bf17b3065293a7c8760b
macRaidev <isaac14e2007@gmail.com>
2026-05-03 15:28:54 -0500

    fix: upper_number from 85% to 95%

--- outlier_filter.py
+++ outlier_filter.py
@@ -4,7 +4,7 @@ def OutlierFilter(data_array):
   sorted_array = sorted(data_array)
 
   # Calculate number at 35% & 85% position
-  lower_number, upper_number= np.percentile(sorted_array, [35, 85])
+  lower_number, upper_number= np.percentile(sorted_array, [35, 95])
 
   # filtering numbers bellow 35% and above 85%
   filtered_array = filter(lambda x: (x > lower_number and x < upper_number), sorted_array)
