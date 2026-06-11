1dc220228e58c3ac275ebaad54ff6b71712a2c09
macRaidev <isaac14e2007@gmail.com>
2026-06-11 10:13:38 -0500

    feat: implement PlayWrightScraping function for dynamic webpage price extraction

--- scraping.py
+++ scraping.py
@@ -1,6 +1,7 @@
+import requests
 from bs4 import BeautifulSoup
 from crawlbase import CrawlingAPI
-import requests
+from playwright.sync_api import sync_playwright
 from dotenv import load_dotenv
 import os
 
@@ -30,6 +31,48 @@ def Scraping(url, html_element, html_class, data_transformer):
     data.append(price_int)
 
   return list(data)
+
+def PlayWrightScraping(url, html_element, html_class, data_transformer):
+  """Scrape prices from a webpage and return as a list of integers."""
+  headers = {
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/5.37.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/5.37.36"
+  }
+  # Extract HTML content
+  with sync_playwright() as p:
+    try:
+      # Launch a headless browser instance
+      browser = p.chromium.launch(headless=True)
+      page = browser.new_page()
+    except Exception as e:
+      print(f"An error occurred while launching the browser: {e}")
+      return []
+    page.goto(url, wait_until="networkidle")
+    
+    # Wait for the specific data or dynamic elements to load into the DOM
+    page.wait_for_selector(f".{html_class}")
+    
+    # Extract the fully rendered HTML content
+    html_content = page.content()
+    browser.close()
+  
+  soup = BeautifulSoup(html_content, "html.parser")
+  print(soup.prettify()) # REMOVE LATER
+  prices = soup.find_all(html_element, class_=html_class)
+
+  # Transform raw HTML text into clean numeric data
+  data = []
+
+  for price in prices:
+    price_text = price.text
+    # Apply the site-specific cleaning function
+    price_text = data_transformer(price_text)
+    price_int = int(float(price_text))
+    data.append(price_int)
+
+  return list(data)
+
+# scrap function using cr awlbase for bypassing anti-scraping
+
 
 # scrap function using crawlbase for bypassing anti-scraping
 load_dotenv()
