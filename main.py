1d69b77a60223d9d8016d0a7f5c5ad35d9a40ed9
macRaidev <isaac14e2007@gmail.com>
2026-06-11 10:13:25 -0500

    feat: add Mercado Libre MX and Amazon US scraping configurations with cleaning functions

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import numpy as np
-from scraping import Scraping, CrawlbaseScrape
+from scraping import Scraping, CrawlbaseScrape, PlayWrightScraping
 from outlier_filter import OutlierFilter
-from cleaning_scraping import mercado_libre_mx as ml_mx
-from cleaning_scraping import amazon_us as am_us
+from sites import mercado_libre_mx as ml_mx
+from sites import amazon_us as am_us
 
 # Configure sites to scrape (URL, HTML element, CSS class, cleaning function)
 sites = [
-  Scraping(ml_mx.url, ml_mx.html_element, ml_mx.html_class, ml_mx.cleaning_function),
+  #Scraping(ml_mx.url, ml_mx.html_element, ml_mx.html_class, ml_mx.cleaning_function), --- SCRAPING DOESN'T WORK, FIX LATER ---
+  
   CrawlbaseScrape(am_us.url, am_us.html_element, am_us.html_class, am_us.cleaning_function),
 ]
 
@@ -19,3 +20,5 @@ for site in sites:
 
 # Calculate average price from filtered data
 np_average = np.mean(total_data)
+
+print(f"Average Price: {np_average}")
